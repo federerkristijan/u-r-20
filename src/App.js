1f63ded2e86d898ff26ee99ebd1a3dc661229cbe
federerkristijan <federer.kristijan@gmail.com>
2022-12-29 23:09:03 +0100

    products route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route } from "react-router-dom";
 
-import Welcome from "./components/Welcome"
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div>
       <Route path="/welcome" >
         <Welcome />
+      </Route>
+      <Route path="/products" >
+        <Products />
       </Route>
     </div>
   );
