b90b8e976687651388662cf635b1a96a70aefff0
federerkristijan <federer.kristijan@gmail.com>
2022-12-30 00:08:56 +0100

    dynamic route not working

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,10 +9,10 @@ const Navbar = () => {
       <nav>
         <ul>
           <li>
-            <NavLink activeClassName={classes.active} to="/welcome">Welcome</NavLink>
+            <NavLink activeclassname={classes.active} to="/welcome">Welcome</NavLink>
           </li>
           <li>
-            <NavLink activeClassName={classes.active} to="/products">Products</NavLink>
+            <NavLink activeclassname={classes.active} to="/products">Products</NavLink>
           </li>
         </ul>
       </nav>
