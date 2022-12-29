8b1ec05abdc81549c4e26f2e8fff98a9fab3227c
federerkristijan <federer.kristijan@gmail.com>
2022-12-29 23:17:02 +0100

    Navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
